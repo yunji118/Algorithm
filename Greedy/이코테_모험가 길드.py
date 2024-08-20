521dec6c2922bc81e48961e4c7674e84e2241b7e
Yunji Ha <yunji118@ewhain.net>
2024-08-20 22:19:46 +0900

    이코테_모험가 길드.py

---
+++ Greedy/이코테_모험가 길드.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+gongpo = list(map(int, input().split()))
+
+gongpo.sort()
+
+ans = 0
+
+num = 0
+i = 0
+while i < N:
+    num += 1   #현재 1명 추가
+
+    if gongpo[i] == num:
+        ans += 1
+        num = 0
+
+    i += 1
+
+print(ans)
